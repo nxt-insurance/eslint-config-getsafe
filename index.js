90ddab0e86c0bc3727cd7f1486d9c15bda3b0b24
yamov <yamov.me@gmail.com>
2019-04-10 11:34:30 +0200

    Remove flowtype/spread-exact-type rule.

--- index.js
+++ index.js
@@ -81,7 +81,6 @@ module.exports = {
     'flowtype/array-style-simple-type': ['error', 'shorthand'],
     'flowtype/boolean-style': ['error', 'boolean'],
     'flowtype/define-flow-type': 'error',
-    'flowtype/spread-exact-type': 'error',
     'flowtype/newline-after-flow-annotation': ['error', 'never'],
     'flowtype/no-dupe-keys': 'error',
     'flowtype/no-primitive-constructor-types': 'error',
