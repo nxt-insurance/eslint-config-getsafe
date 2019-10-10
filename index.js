efbad7fbcb78921e0eb9a9c65d9d9d28234015af
Benjamin Arbogast <b.arbogast@getsafe.de>
2019-10-10 13:51:31 +0200

    Extract configuration for flow into separate file
    This should allow to easier exlcude rules for flow for non flow files.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: ['flowtype', 'import', 'prettier', 'promise', 'react', 'react-hooks'],
-  extends: ['plugin:flowtype/recommended', 'plugin:react/recommended', 'airbnb', 'prettier'],
+  plugins: ['import', 'prettier', 'promise', 'react', 'react-hooks'],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   rules: {
     'prettier/prettier': [
       'error',
@@ -79,46 +79,5 @@ module.exports = {
 
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
-
-    'flowtype/array-style-complex-type': ['error', 'shorthand'],
-    'flowtype/array-style-simple-type': ['error', 'shorthand'],
-    'flowtype/boolean-style': ['error', 'boolean'],
-    'flowtype/define-flow-type': 'error',
-    'flowtype/newline-after-flow-annotation': ['error', 'never'],
-    'flowtype/no-dupe-keys': 'error',
-    'flowtype/no-primitive-constructor-types': 'error',
-    'flowtype/no-types-missing-file-annotation': ['error'],
-    'flowtype/no-unused-expressions': ['error'],
-    'flowtype/no-weak-types': 'error',
-    'flowtype/require-exact-type': ['error', 'always'],
-    'flowtype/require-return-type': [
-      'error',
-      'always',
-      {
-        annotateUndefined: 'never',
-        excludeArrowFunctions: 'expressionsOnly',
-      },
-    ],
-    'flowtype/require-types-at-top': 'error',
-    'flowtype/require-valid-file-annotation': [
-      'error',
-      'always',
-      {
-        annotationStyle: 'line',
-      },
-    ],
-    'flowtype/type-id-match': ['error', '^([A-Z]+[a-z0-9A-Z]*)$'],
-    'flowtype/type-import-style': ['error', 'declaration'],
-    'flowtype/use-flow-type': 'error',
-    'flowtype/valid-syntax': 'error',
-    // Disable stylistic rules. Prettier takes care of formatting.
-    'flowtype/delimiter-dangle': 'off',
-    'flowtype/generic-spacing': 'off',
-    'flowtype/object-type-delimiter': 'off',
-    'flowtype/semi': 'off',
-    'flowtype/space-after-type-colon': 'off',
-    'flowtype/space-before-generic-bracket': 'off',
-    'flowtype/space-before-type-colon': 'off',
-    'flowtype/union-intersection-spacing': 'off',
   },
 }
