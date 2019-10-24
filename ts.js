2ff199fbf6e2394bf35a95924e84467db0dd23b7
Digvijay Upadhyay <d.upadhyay@planfwd.de>
2019-10-24 16:41:20 +0200

    chore: remove flow type from typescript config

--- ts.js
+++ ts.js
@@ -8,7 +8,6 @@ module.exports = {
     require.resolve('./rules/import.js'),
     require.resolve('./rules/promise.js'),
     require.resolve('./rules/react-hooks.js'),
-    require.resolve('./rules/flowtype.js'),
     require.resolve('./rules/prettier.js'),
   ],
   rules: {
