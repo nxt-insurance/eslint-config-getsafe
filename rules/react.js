3cf689a60df82bcca54a68a2d76463e663dd60f5
yamov <yamov.me@gmail.com>
2020-01-08 23:17:07 +0100

    Update react rules

--- rules/react.js
+++ rules/react.js
@@ -3,19 +3,19 @@ module.exports = {
   extends: ['plugin:react/recommended'],
   rules: {
     'react/destructuring-assignment': 'off',
-    // override this rule if using extension .jsx or .tsx
-    'react/jsx-filename-extension': ['error', { extensions: ['.js, .jsx, .tsx'] }],
+    'react/display-name': 'off',
+    'react/forbid-foreign-prop-types': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.tsx'] }],
     'react/jsx-props-no-spreading': 'off',
     'react/no-danger': 'off',
     'react/prop-types': 'off',
     'react/require-default-props': 'off',
     'react/state-in-constructor': ['error', 'never'],
+    'react/static-property-placement': 'off',
     // Disable stylistic rules. Prettier takes care of formatting.
     'react/jsx-wrap-multilines': 'off',
     'react/jsx-indent': 'off',
     'react/jsx-one-expression-per-line': 'off',
     'react/jsx-curly-newline': 'off',
-    'react/display-name': 'off',
-    'react/forbid-foreign-prop-types': 'off',
   },
 }
