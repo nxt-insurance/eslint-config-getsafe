c28e6e7e23ee8b468dc3c3b3e91d4f9559ed6f54
Benjamin Arbogast <b.arbogast@getsafe.de>
2020-02-28 13:23:20 +0100

    Turn off @typescript-eslint/member-delimiter-style
    The current configuration conflicts with prettier. Let's turn it off, prettier will do the right thing.

--- rules/typescript.js
+++ rules/typescript.js
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/array-type': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/member-delimiter-style': "off",
     '@typescript-eslint/no-extra-non-null-assertion': 'error',
     '@typescript-eslint/no-extraneous-class': 'error',
     '@typescript-eslint/no-parameter-properties': 'error',
@@ -13,19 +14,6 @@ module.exports = {
     '@typescript-eslint/no-useless-constructor': 'error',
     '@typescript-eslint/prefer-for-of': 'error',
     '@typescript-eslint/prefer-optional-chain': 'error',
-    '@typescript-eslint/member-delimiter-style': [
-      'error',
-      {
-        multiline: {
-          delimiter: 'none',
-          requireLast: false,
-        },
-        singleline: {
-          delimiter: 'comma',
-          requireLast: false,
-        },
-      },
-    ],
     // rules which requires parserServices to be generated, enable later
     // '@typescript-eslint/no-floating-promises': 'error',
     // '@typescript-eslint/no-throw-literal': 'error',
